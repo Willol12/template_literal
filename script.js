188999ae5bcdbc468443e40538e571ab500ef0bf
willol12 <ccossuol@outlook.com>
2020-08-19 18:27:08 -0300

    template_literal

---
+++ script.js
@@ -0,0 +1,19 @@
+//Object {}
+//Bolean true, false
+//String '', ""
+//Template literal ${}
+
+const mensagem = `Oi você ai essa é minha 
+'primeira' mensage`;
+
+const nome =`Willian`;
+
+const email =
+`Olá ${nome} você é ${5+5}
+
+Obrigado por se cadastrar no curso online
+Espero que esteja gostando do assunto
+E bora codar
+Obrigado,
+Willian`;
+console.log(email)
